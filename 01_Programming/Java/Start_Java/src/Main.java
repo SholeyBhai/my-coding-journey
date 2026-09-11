d7a2e2ff8b34db85fb88f59ab6d64e73f9289f67
Rampratap Yadav <Rampratapyadav9569@gmail.com>
2026-09-11 23:48:38 +0530

    Day 2: Start Java

---
+++ 01_Programming/Java/Start_Java/src/Main.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.println("i = " + 1);
+        System.out.println("Rampratap Yadav");
+        System.out.println("i = " + 2);
+        System.out.println("Rampratap Yadav");
+        System.out.println("i = " + 3);
+        System.out.println("Rampratap Yadav");
+        System.out.println("i = " + 4);
+        System.out.println("Rampratap Yadav");
+        System.out.println("i = " + 5);
+        System.out.println("Rampratap Yadav");
+        /* lets learn variable , int , float , boolean */
+        String name="Rampratap";
+                System.out.println(name);
+                int a = 45;
+                        float b =45.33f;
+                                boolean isAdult=false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(isAdult);
+        /* Rules for constructing name of variables in Java
+        1. Can contain digits, underscore, dollar signs, letters
+        2. Should begin with a letter , $ or _
+        3. Java is case sensitive langauge which means that ram and Ram are two  different
+     variable together
+        4. Should not contain whitespaces
+         5. you can not use reserved keywords from java
+         */
+
+    }
+}
