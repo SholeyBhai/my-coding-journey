60c850ac9d0f5c0d2b78960d6d1c4150db09a2c3
Rampratap Yadav <Rampratapyadav9569@gmail.com>
2026-09-15 23:38:06 +0530

    Day 5: Java while loop

---
+++ 01_Programming/Java/Start_Java/Day_05/Main.java
@@ -0,0 +1,11 @@
+switch (week);
+        { case 1;
+            system.out.printlm("Sunday");
+        }
+                {
+case 2;
+system.out.println("Monday");
+}
+{
+case 3;}
+system.out.println("Tuesday")
---
+++ 01_Programming/Java/Start_Java/Day_05/whileloop.java
@@ -0,0 +1,13 @@
+public class whileloop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("Loop finished!");
+    }
+}
