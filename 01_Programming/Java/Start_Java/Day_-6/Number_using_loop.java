64c5508b06a5be617138647aee28b58ad249841b
Rampratap Yadav <Rampratapyadav9569@gmail.com>
2026-09-16 23:40:51 +0530

    Practice_loop

---
+++ 01_Programming/Java/Start_Java/Day_-6/Number_using_loop.java
@@ -0,0 +1,10 @@
+public class Number_using_loop {
+    public static void main(String[] args ){
+        int n=10;
+        while(n>=1){
+            System.out.println(n);
+            n--;
+
+        }
+    }
+}
